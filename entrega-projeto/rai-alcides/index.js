be633df7775c65b705bc22906504ffcf9394338e
Rai Alcides <railayli2@hotmail.com>
2022-10-29 16:11:42 -0300

    segunda parte do exercicio

--- entrega-projeto/rai-alcides/index.js
+++ entrega-projeto/rai-alcides/index.js
@@ -4,16 +4,23 @@ const button = document.querySelector('.search-button')
 
 button.addEventListener("click", (event) => {
    event.preventDefault()
-   const username = input.value
+   const username = input.value.trim()
+   username ? getGitHubUser(username) : alert("Digite uma usuaria valida")
    getGitHubUser(username)
+   input.value = ""
 })
 
 getGitHubUser = async (user) => {
    try {
      const response = await fetch(`https://api.github.com/users/${user}`)
      const userData = await response.json()
-     console.log(user)
-     createCard(userData)
+      if (response.status == 404) {
+        renderUserNotFound()
+      } else if (response.status = 200){
+        createCard(userData)
+      }
+     
+     
    } catch (error) {
     console.error('capiturei um erro:', error) 
    }
@@ -23,10 +30,64 @@ createCard = (user) => {
     const {avatar_url, name, login, bio, followers, public_repos, } = user
     main.innerHTML = `
       <div class="card">
-      <img class="profile-img" src=${avatar_url} alt="">
+       <img class="profile-img" src=${avatar_url} alt="">
+       <h2 class="profile-title">${name}</h2>
+       <h4 class="profile-subtitle">${login}</h4>
+       <p class="profile-description">${bio ? bio : ""}</p>
+       <div class="profile-infos">
+         <div class="info-box">
+           <img src="../../assets/people_outline.png" alt="">
+           <p>${followers}</p>
+         </div>
+         <a class="link-repositorio">
+          <div class="info-box">
+           <img src="../../assets/Vector.png" alt="">
+           <p>${public_repos}</p>
+         </div>
+        </a>
+       </div>
       </div>
+   `
+   const linkRepositores = document.querySelector(".link-repositorio")
+
+   linkRepositores.addEventListener('click', (evento) => {
+       evento.preventDefault()
+       getRepositorios(login)
+   })
+   
+}
+
+getRepositorios = async (user) => {
+    try {
+        const response = await fetch(`https://api.github.com/users/${user}/repos`)
+        const repos = await response.json()
+         if (repos.length > 0) {
+           creatRepositoresCard(repos)
+         } else {
+            renderNotFoundRepositores()
+         }
+        
+        
+      } catch (error) {
+       console.error('capiturei um erro:', error) 
+      }
+}
+
+
+
+renderUserNotFound = () => {
+    return main.innerHTML = `
+    <div class="not-foud">
+      <h2 class="not-foud-title">Usuaria não encontrada</h2>
+      <h4 class="not-foud-subtitulo">Pesquise novamente</h4>
+      <img src="../../assets/notfound.png" alt="">
+    </div>
+    
     `
 }
+
+
+
 
 
 
